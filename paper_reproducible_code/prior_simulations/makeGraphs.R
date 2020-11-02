e7a0f4eaad55143fbeb320f496385c97f0278342
salleuska <sally.paganin@berkeley.edu>
2020-11-02 17:20:08 -0500

    added graphs from priors

--- paper_reproducible_code/prior_simulations/makeGraphs.R
+++ paper_reproducible_code/prior_simulations/makeGraphs.R
@@ -1,4 +1,12 @@
 library(ggplot2)
+## Global ggplot theme
+theme_set(theme_bw() +
+          theme(axis.text = element_text(size= 16),
+                axis.title=element_text(size=18),
+                strip.text = element_text(size=18),
+                strip.background =element_rect(fill="white"),
+                plot.title = element_text(size=16), 
+                legend.text=element_text(size=16)))
 
 outdir <- "graphs/"
 ########################################
@@ -17,39 +25,21 @@ for(i in 1:length(listFiles)) {
 
 colnames(priorSamples) <- gsub("priorSamples_|\\.rds", "", listFiles)
 
-########################################
-## samples from bnp models
-########################################
-dir <- "simulated_samples/bnp/"
-
-listFiles <- list.files(dir)
-
-priorSamples  <- matrix(0, ncol = length(listFiles), nrow = 10*100*100)
-
-for(i in 1:length(listFiles)) {
-	ff <- readRDS(paste0(dir, listFiles[i]))
-	priorSamples[,i] <- as.numeric(ff)
-}
-
-colnames(priorSamples) <- gsub("priorSamples_|\\.rds", "", listFiles)
-str(priorSamples)
-
-##########################################
 dd <- reshape2::melt(priorSamples)
 str(dd)
 
-apply(priorSamples, 2, function(x) density(x)$bw)
-
-p_hist <- ggplot(dd, aes(x= value, col = Var2)) + 
+p_hist <- ggplot(dd, aes(x= value)) + 
                geom_histogram(aes(y = ..density..),  
-               binwidth=0.03, fill = "white") +
+               bins = 50, fill = "white", col = "gray50") +
                facet_wrap(.~ Var2)  +
                theme_bw()  + theme(legend.position = 'none') +
-               xlab("Probability") + ylab("") +
+               xlab("") + ylab("") +
+               ggtitle("Distribution of probabilities samples - parametric models") + 
                theme(strip.background = element_rect(color="black", 
-               	fill="white", size=1, linetype="solid"))
+                fill="white", linetype="solid"))
 
 p_hist
+
 
 p_hist <- p_hist  + ylim(0, 8) + stat_function(fun = dbeta, colour="black", lty = 2,  n = 200, xlim = c(0.0001, 0.9999),
                       args = list(shape1 = 0.5, shape2 = 0.5)) 
@@ -74,14 +64,172 @@ dd_qq <- reshape2::melt(qq_df, id = "y")
 str(dd_qq)
 # levels(dd_qq$variable) <- paste0("alpha = ", levels(dd_qq$variable))
 
-p <- ggplot(dd_qq, aes(sample= value, col = variable)) + 
-               stat_qq(distribution = qbeta, dparams = c(0.5, 0.5)) +
+p <- ggplot(dd_qq, aes(sample= value)) +
+               scale_y_continuous(breaks = seq(0.0, 1.0, length = 5)) + 
+               scale_x_continuous(breaks = seq(0.0, 1.0, length = 5)) + 
+               stat_qq(distribution = qbeta, dparams = c(0.5, 0.5), col = "gray") +
                stat_qq_line(distribution = qbeta, dparams = c(0.5, 0.5), col = 1) +
-               xlab("theoretical - Beta(0.5, 0.5)") + ylab("Empirical") +
+               xlab("Theoretical quantiles - Beta(0.5, 0.5)") + ylab("Empirical quantiles") +
                facet_wrap(.~ variable) +   
-               theme_bw()  + theme(legend.position = 'none')
+               theme_bw()  + theme(legend.position = 'none')+
+               theme(strip.background = element_rect(color="black", 
+                fill="white", linetype="solid"))
 
 p  
 
 ggsave(filename = paste0(outdir, modelType, "_qqplot.png"), plot = p,
         width = 8, height = 6, dpi = 400, units = "in", device='png')
+
+########################################
+## samples from bnp models
+########################################
+dir <- "simulated_samples/bnp/"
+modelType <- "bnp"
+
+listFiles <- list.files(dir)
+# listFiles <- listFiles[c(3, 6, 9, 12)]
+
+priorSamples  <- matrix(0, ncol = length(listFiles), nrow = 10*100*100)
+
+for(i in 1:length(listFiles)) {
+	ff <- readRDS(paste0(dir, listFiles[i]))
+	priorSamples[,i] <- as.numeric(ff)
+}
+
+colnames(priorSamples) <- gsub("priorSamples_|\\.rds", "", listFiles)
+str(priorSamples)
+
+##########################################
+dd <- reshape2::melt(priorSamples)
+str(dd)
+
+if(length(levels(dd$Var2)) == 4) levels(dd$Var2) <- c("IRT_constrainedItem", "IRT_unconstrained" ,"SI_constrainedItem","SI_unconstrained")
+
+## binwidth=0.03,
+p_hist <- ggplot(dd, aes(x= value)) + 
+               geom_histogram(aes(y = ..density..),  
+               bins = 50, col = "gray50", fill = "white") +
+               facet_wrap(.~ Var2)  +
+               theme_bw()  + theme(legend.position = 'none') +
+               xlab("") + ylab("") +
+               ggtitle("semiparametric models") + 
+               theme(strip.background = element_rect(color="black", 
+               	fill="white", linetype="solid"))
+
+p_hist
+
+
+p_hist <- p_hist  + ylim(0, 8) + stat_function(fun = dbeta, colour="black", lty = 2,  n = 200, xlim = c(0.0001, 0.9999),
+                      args = list(shape1 = 0.5, shape2 = 0.5)) 
+p_hist
+
+ggsave(filename = paste0(outdir, modelType, "_dist.png"), plot = p_hist,
+        width = 8/3*2, height = 6, dpi = 400, units = "in", device='png')
+
+################
+## qqplots 
+################
+qq_df <- apply(priorSamples, 2,  sort)
+qq_df <- as.data.frame(qq_df)
+
+qq_df$y <- qbeta(ppoints(length(qq_df[,1])), 0.5, 0.5) 
+
+vv <- round(seq.int(1, dim(qq_df)[1], length = 5000))
+qq_df <- qq_df[vv, ]
+str(qq_df)
+
+dd_qq <- reshape2::melt(qq_df, id = "y")
+str(dd_qq)
+# levels(dd_qq$variable) <- paste0("alpha = ", levels(dd_qq$variable))
+
+p <- ggplot(dd_qq, aes(sample= value)) + ylim(-0.1,1.1)+
+               scale_y_continuous(breaks = seq(0.0, 1.0, length = 5)) + 
+               scale_x_continuous(breaks = seq(0.0, 1.0, length = 5)) + 
+               stat_qq(distribution = qbeta, dparams = c(0.5, 0.5), col = "gray") +
+               stat_qq_line(distribution = qbeta, dparams = c(0.5, 0.5), col = 1) +
+               xlab("Theoretical quantiles - Beta(0.5, 0.5)") + ylab("Empirical quantiles") +
+               facet_wrap(.~ variable) +   
+               theme_bw()  + theme(legend.position = 'none')+
+               theme(strip.background = element_rect(color="black", 
+                fill="white", linetype="solid"))
+
+p  
+
+ggsave(filename = paste0(outdir, modelType, "_qqplot.png"), plot = p,
+        width = 8, height = 6, dpi = 400, units = "in", device='png')
+
+
+########################################
+## samples from bnp models - fixed alpha
+########################################
+dir <- "simulated_samples/bnpfixedAlpha/"
+modelType <- "bnpFixedAlpha"
+
+listFiles <- list.files(dir)
+# listFiles <- listFiles[c(3, 6, 9, 12)]
+
+priorSamples  <- matrix(0, ncol = length(listFiles), nrow = 10*100*100)
+
+for(i in 1:length(listFiles)) {
+  ff <- readRDS(paste0(dir, listFiles[i]))
+  priorSamples[,i] <- as.numeric(ff)
+}
+
+colnames(priorSamples) <- gsub("priorSamples_|\\.rds", "", listFiles)
+str(priorSamples)
+
+##########################################
+dd <- reshape2::melt(priorSamples)
+str(dd)
+
+p_hist <- ggplot(dd, aes(x= value)) + 
+               geom_histogram(aes(y = ..density..),  
+               bins = 50, col = "gray50", fill = "white") +
+               facet_wrap(.~ Var2, ncol = 6)  +
+               theme_bw()  + theme(legend.position = 'none') +
+               xlab("") + ylab("") +
+               ggtitle("Distribution of probabilities samples") + 
+               theme(strip.background = element_rect(color="black", 
+                fill="white", linetype="solid"))
+
+p_hist
+
+
+p_hist <- p_hist  + ylim(0, 8) + stat_function(fun = dbeta, colour="black", lty = 2,  n = 200, xlim = c(0.0001, 0.9999),
+                      args = list(shape1 = 0.5, shape2 = 0.5)) 
+p_hist
+
+ggsave(filename = paste0(outdir, modelType, "_dist.png"), plot = p_hist,
+        width = 14, height = 10, dpi = 400, units = "in", device='png')
+
+################
+## qqplots 
+################
+qq_df <- apply(priorSamples, 2,  sort)
+qq_df <- as.data.frame(qq_df)
+
+qq_df$y <- qbeta(ppoints(length(qq_df[,1])), 0.5, 0.5) 
+
+vv <- round(seq.int(1, dim(qq_df)[1], length = 5000))
+qq_df <- qq_df[vv, ]
+str(qq_df)
+
+dd_qq <- reshape2::melt(qq_df, id = "y")
+str(dd_qq)
+# levels(dd_qq$variable) <- paste0("alpha = ", levels(dd_qq$variable))
+
+p <- ggplot(dd_qq, aes(sample= value)) + 
+               scale_y_continuous(breaks = seq(0.0, 1.0, length = 5)) + 
+               scale_x_continuous(breaks = seq(0.0, 1.0, length = 5)) + 
+               stat_qq(distribution = qbeta, dparams = c(0.5, 0.5), col = "gray") +
+               stat_qq_line(distribution = qbeta, dparams = c(0.5, 0.5), col = 1) +
+               xlab("Theoretical quantiles - Beta(0.5, 0.5)") + ylab("Empirical quantiles") +
+               facet_wrap(.~ variable, ncol = 6) +   
+               theme_bw()  + theme(legend.position = 'none')+
+               theme(strip.background = element_rect(color="black", 
+                fill="white", linetype="solid"))
+
+p  
+
+ggsave(filename = paste0(outdir, modelType, "_qqplot.png"), plot = p,
+        width = 14, height = 10, dpi = 400, units = "in", device='png')
