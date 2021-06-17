15de9037c6598473478d62d8abeb69265b83ee90
salleuska <sallypaganin@gmail.com>
2021-06-17 17:19:02 -0400

    update main.sh for stan models

--- paper_reproducible_code/main.sh
+++ paper_reproducible_code/main.sh
@@ -118,19 +118,114 @@ Rscript 1_runModels.R  \
 --mode=centered
 
 #############################################
+## TIMSS DATA 
+#############################################
+## Parametric models
+
+FILES=(models/parametric_long/*.R)
+
+Rscript 1_runModels.R  \
+--model=${FILES[$SLURM_ARRAY_TASK_ID]} \
+--data=data/data_timss.rds \
+--niter=50000 \
+--nburnin=5000 \
+--nthin=10 \
+--mode=default
+
+Rscript 1_runModels.R  \
+--model=${FILES[$SLURM_ARRAY_TASK_ID]} \
+--data=data/data_timss.rds \
+--niter=50000 \
+--nburnin=5000 \
+--nthin=10 \
+--mode=centered
+
+## Semiparametric models
+
+FILES=(models/bnp_long/*.R)
+
+Rscript 1_runModels.R  \
+--model=${FILES[$SLURM_ARRAY_TASK_ID]} \
+--data=data/data_timss.rds \
+--niter=50000 \
+--nburnin=5000 \
+--nthin=10 \
+--mode=default
+
+Rscript 1_runModels.R  \
+--model=${FILES[$SLURM_ARRAY_TASK_ID]} \
+--data=data/data_timss.rds \
+--niter=50000 \
+--nburnin=5000 \
+--nthin=10 \
+--mode=centered
+
+#############################################
+## HEALTH DATA 
+#############################################
+## Parametric models
+
+FILES=(models/parametric/*.R)
+
+Rscript 1_runModels.R  \
+--model=${FILES[$SLURM_ARRAY_TASK_ID]} \
+--data=data/data_health.rds \
+--niter=50000 \
+--nburnin=5000 \
+--nthin=10 \
+--mode=default
+
+Rscript 1_runModels.R  \
+--model=${FILES[$SLURM_ARRAY_TASK_ID]} \
+--data=data/data_health.rds \
+--niter=50000 \
+--nburnin=5000 \
+--nthin=10 \
+--mode=centered
+
+## Semiparametric models
+
+FILES=(models/bnp/*.R)
+
+Rscript 1_runModels.R  \
+--model=${FILES[$SLURM_ARRAY_TASK_ID]} \
+--data=data/data_health.rds \
+--niter=50000 \
+--nburnin=5000 \
+--nthin=10 \
+--mode=default
+
+Rscript 1_runModels.R  \
+--model=${FILES[$SLURM_ARRAY_TASK_ID]} \
+--data=data/data_health.rds \
+--niter=50000 \
+--nburnin=5000 \
+--nthin=10 \
+--mode=centered
+
+#############################################
 ## Stan models
 #############################################
 
 Rscript 1_runStanModel.R  \
 --data=data/simulation_unimodal.rds \
---niter=4000 \
---nburnin=4000 
+--nsamples=4000 \
+--nwarmup=4000 
 
 Rscript 1_runStanModel.R  \
 --data=data/simulation_bimodal.rds \
---niter=4000 \
---nburnin=4000 
+--nsamples=4000 \
+--nwarmup=4000 
 
+Rscript 1_runStanModel.R  \
+--data=data/data_health.rds \
+--nsamples=4000 \
+--nwarmup=4000 
+
+Rscript 1_runStanModel.R  \
+--data=data/data_timss.rds \
+--nsamples=4000 \
+--nwarmup=4000 
 #############################################
 ## 2) extract MCMC samples and postprocess them
 #############################################
@@ -146,9 +241,20 @@ for filename in output/posterior_samples/simulation_bimodal/parametric/*.rds; do
 	echo $filename
 	Rscript 2_extractResults.R --resFileName=$filename
 done
+
+for filename in output/posterior_samples/data_timss/parametric/*.rds; do
+	echo $filename
+	Rscript 2_extractResults.R --resFileName=$filename
+done
+
+for filename in output/posterior_samples/data_health/parametric/*.rds; do
+	echo $filename
+	Rscript 2_extractResults.R --resFileName=$filename
+done
+
 ############################################################
 ## Extract results for semiparametric models 
- 
+
 for filename in output/posterior_samples/simulation_unimodal/bnp/*.rds; do
 	echo $filename
 	Rscript 2_extractResults.R --resFileName=$filename
@@ -158,6 +264,18 @@ for filename in output/posterior_samples/simulation_bimodal/bnp/*.rds; do
 	echo $filename
 	Rscript 2_extractResults.R --resFileName=$filename
 done
+
+for filename in output/posterior_samples/data_timss/bnp/*.rds; do
+	echo $filename
+	Rscript 2_extractResults.R --resFileName=$filename
+done
+
+
+for filename in output/posterior_samples/data_health/bnp/*.rds; do
+	echo $filename
+	Rscript 2_extractResults.R --resFileName=$filename
+done
+
 
 #############################################
 ## 3) Simulate from DP measure 
@@ -180,3 +298,11 @@ Rscript 3_simulateFromDPmeasure.R \
 # Rscript 3_simulateFromDPmeasure.R \
 # --dataName="data_timss" \
 # --modelName="bnp_IRT_unconstrained"
+
+#############################################
+## 4) Make plots
+############################################
+
+
+
+
